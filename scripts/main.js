4ff344cda59d0bcd9ee0705eb9def233f79150d3
lourdesmalenaabate <lourdesmalenaabate@gmail.com>
2021-03-16 17:24:33 -0300

    Funcionalidad menu hambuguesa

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
 //Funcionalidad Proyecto
+const mnuBtn =document.querySelector('nav .button')
+const mnuLst = document.querySelector('nav .menu')
 
+mnuBtn.onclick = () => mnuLst.classList.toggle('active')
 //Autor: Malena Abate
